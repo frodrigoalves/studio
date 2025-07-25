d701ecbeb987ee583f81ce266142c0e4133193b8
Rodrigo Alves <f.rodrigoalves12@gmail.com>
2025-07-25 03:26:01 +0000

    Content Security Policy of your site blocks the use of 'eval' in JavaScr

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,19 @@
+
 import type {NextConfig} from 'next';
+
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data: https://placehold.co https://firebasestorage.googleapis.com;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -21,6 +36,19 @@ const nextConfig: NextConfig = {
         hostname: 'firebasestorage.googleapis.com',
       }
     ],
+  },
+   async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ]
   },
 };
 
