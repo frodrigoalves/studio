e4ed6d4059a74c44163c843516186c0dbe211ae7
Rodrigo Alves <f.rodrigoalves12@gmail.com>
2025-08-09 06:25:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/tire-form.tsx
+++ src/components/tire-form.tsx
@@ -14,6 +14,7 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
+  FormDescription,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
